0eeb14f3565d1490bf0a2d9662ba83b769721c06
blofstedt <38229215+blofstedt@users.noreply.github.com>
2026-01-28 14:41:50 -0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,13 @@
-
+// src/lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xqleyxrftyehodksashu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxbGV5eHJmdHllaG9ka3Nhc2h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0NDEyNDAsImV4cCI6MjA4NTAxNzI0MH0.d_AFPOGwCAIy8eui6hREyC8uk_iVSL_3IGNVDG9f8XI';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || process.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || process.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(SUPABASE_URL as string, SUPABASE_ANON_KEY as string, {
   auth: {
-    autoRefreshToken: true,
     persistSession: true,
+    autoRefreshToken: true,
     detectSessionInUrl: true
   }
 });
