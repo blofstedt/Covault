cb73852890733fd62c1763b9de8974bbba1b127f
blofstedt <38229215+blofstedt@users.noreply.github.com>
2026-01-30 14:31:55 -0700

    Update DashboardSettingsModal.tsx

--- components/dashboard_components/DashboardSettingsModal.tsx
+++ components/dashboard_components/DashboardSettingsModal.tsx
@@ -6,7 +6,7 @@ import DiscretionaryShieldSection from './settings_modal_components/Discretionar
 import VaultSharingSection from './settings_modal_components/VaultSharingSection';
 import SupportFeedbackSection from './settings_modal_components/SupportFeedbackSection';
 import SignOutSection from './settings_modal_components/SignOutSection';
-import NotificationSettingsSection from './dashboard_components/settings_modal_components/NotificationSettingsSection;
+import NotificationSettingsSection from './settings_modal_components/NotificationSettingsSection';
   
 export interface DashboardSettings {
   theme: string;
